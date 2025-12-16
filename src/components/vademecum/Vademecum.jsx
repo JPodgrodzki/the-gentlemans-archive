547ecb82fbcc16b3dc2e04649611d9612fa7626e
Jakub Podgrodzki <podgrodzkijakub@gmail.com>
2025-12-16 23:24:35 +0100

    Vademecum first steps

--- src/components/vademecum/Vademecum.jsx
+++ src/components/vademecum/Vademecum.jsx
@@ -1,9 +1,71 @@
 import './vademecum.css';
+import collars from '../../assets/images/vademecum/collars.PNG';
 
 export const Vademecum = ({ year }) => {
     return (
         <div className="vademecum">
-            Vademecum to year {year}
+            <div className="article">
+                <div className="article__photo">
+                    <img src={collars} alt="category" className='article__photo--img' />
+                </div>
+                <div className="article__title">
+                    Collars
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Suits
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Hats
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Coats
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Gallantry
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Jewellery
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Accessories
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Shoes
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Watches
+                </div>
+            </div>
+            <div className="article">
+                <div className="article__photo"></div>
+                <div className="article__title">
+                    Bags
+                </div>
+            </div>
         </div>
     )
 }
